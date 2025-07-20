7f87992a968810b6a954db61b2c1908cce2b2590
Ahmed Al-Kanan <ahmed.al-kanan@id.thesoul.io>
2025-07-20 19:00:47 +0200

    setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,14 @@
 const { defineConfig } = require("cypress");
 
+const baseUrl = 'https://ua.sinoptik.ua';
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://ua.sinoptik.ua',
+    baseUrl,
     viewportWidth: 1280,
     viewportHeight: 800,
     setupNodeEvents(on, config) {
+      config.env.environment = 'qa'
+      return config;
       // implement node event listeners here
     },
   },
